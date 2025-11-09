2e4fb47dde5690899127af198b69d79cef47003d
mehi raj <mehiraj8877@gmail.com>
2025-11-09 16:14:52 +0530

    Create loop_with_break.py

---
+++ loop_with_break.py
@@ -0,0 +1,9 @@
+# 🧩 Program: Using break statement inside a loop
+
+for i in range(1, 11):
+    if i == 6:
+        print("🚫 Loop stopped early at 6!")
+        break
+    print(i)
+
+print("✅ Loop ended.")
